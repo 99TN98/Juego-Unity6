40d24ba76df5e2977faff65c5d0baba0ce96a0ba
99TN98 <489201v@gmail.com>
2025-10-12 18:34:22 -0300

    GeneradorDiaNoche
    Empleado con Corrutinas

---
+++ Juego x Unity6LTS/Assets/Scripts/GeneradorDiaNoche.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;  
+
+public class GeneradorDiaNoche : MonoBehaviour
+{
+    [SerializeField] private Light2D luzGlobal;  
+    [SerializeField] private Color nocheColor;
+    [SerializeField] private Color diaColor;
+    [SerializeField] private float segundos;
+    [SerializeField] private float duracionTransicion = 2f;  
+
+    private bool esDeNoche = false;
+
+    void Start()
+    {
+        
+        if (diaColor == null)
+        {
+            diaColor = luzGlobal.color;
+        }
+
+       
+        StartCoroutine(CambiarColorGradualmente());
+    }
+
+    IEnumerator CambiarColorGradualmente()
+    {
+        while (true)
+        {
+            
+            Color objetivoColor = esDeNoche ? nocheColor : diaColor;
+
+           
+            float tiempoTranscurrido = 0f;
+            Color colorInicial = luzGlobal.color;
+
+            
+            while (tiempoTranscurrido < duracionTransicion)
+            {
+                tiempoTranscurrido += Time.deltaTime;
+                luzGlobal.color = Color.Lerp(colorInicial, objetivoColor, tiempoTranscurrido / duracionTransicion);
+                yield return null;  
+            }
+
+            
+            luzGlobal.color = objetivoColor;
+
+            
+            esDeNoche = !esDeNoche;
+
+           
+            yield return new WaitForSeconds(segundos);
+        }
+    }
+}
